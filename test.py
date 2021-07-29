3b47b98c7310d51472c9e50e0aa25a525da630f0
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-07-29 17:11:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,16 @@
-percentageWhite = 0.11
+percentageWhite = 0.1
 actualHeight = 20#cm
 actualSideLength = 100#cm
 cubicVol = actualHeight*actualSideLength**2
 pyramidalVol = (cubicVol*1.5-10*10*10)*1/3
 totalVolume = cubicVol+pyramidalVol
 cubicThreshold = 0.6
-pyramidalThreshold = 0.1
 if percentageWhite > cubicThreshold:
     #20cm height
     #rate of decrease
     percentageHeight = ((percentageWhite-cubicThreshold)/(1-cubicThreshold))**(1/2)
     print((percentageHeight*actualHeight*actualSideLength**2+pyramidalVol)/totalVolume)
-elif percentageWhite > pyramidalThreshold:
+else:
     percentagePyramidal = (percentageWhite/cubicThreshold)
     # 2a^2+b^2 = c^2 --> k(2a^2+b^2)^(1/2)= k c #a^2*b
     print(pyramidalVol*(percentagePyramidal)/totalVolume)
-else:
-    print(0)
