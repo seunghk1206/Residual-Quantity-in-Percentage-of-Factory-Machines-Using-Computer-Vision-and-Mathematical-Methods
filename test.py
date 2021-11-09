23d4ca5a3d822060edc4f3c22c746cd921b808f9
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-11-09 21:31:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 def PhotoAnalysis(actualHeight, actualSideLength, cubicThreshold):
     percentageWhite = 0.8 # 사진에서의 하얀/회색 부분의 퍼센트.
     cubicVol = actualHeight*actualSideLength**2 # 깔때기의 직육면체부분의 부피.
@@ -16,3 +17,8 @@ actualHeight = 24 # 깔때기 직육면체 부분의 높이 센티미터로
 actualSideLength = 68 # 깔때기 직육면체 가로 세로 길이. 정사각형이라고 들었기에 1개 뿐
 cubicThreshold = 0.8 # 깔때기의 피라미드 부분만이 찼을때 찍힌 사진의 퍼센트. 예: 0.6이면 사진에서 60%가 깔때기의 물체로 차있게 보인다면, 그것은 깔때기의 피라미드 부분을 전부 채운 것이라는 가정을 한다.
 print(PhotoAnalysis(actualHeight, actualSideLength, cubicThreshold)) # 함수 불러오기. 구체적 정보는 PhotoKMeanFunc.py 참조바람
+'''
+
+from PIL import Image
+img = Image.open('FronTechData/50-2.jpg').convert('L')
+img.save('greyscale50-2.png')
