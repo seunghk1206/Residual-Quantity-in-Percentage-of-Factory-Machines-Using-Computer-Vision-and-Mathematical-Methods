51b7f369bb5e72ffd0dfc83e85d9a509ed663333
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-11-22 20:46:09 +0900

    Update PhotoKMeanFunc.py

--- PhotoKMeanFunc.py
+++ PhotoKMeanFunc.py
@@ -4,35 +4,17 @@ import cv2 as cv
 def PhotoAnalysis(Dir):
     img = cv.imread(Dir)
     lp = len(img)
-    Z = img.reshape((-1,3))
-    # convert to np.float32
-    Z = np.float32(Z)
-    # define criteria, number of clusters(K) and apply kmeans()
-    '''
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 10
-    ret,label,center=cv.kmeans(Z,K,None,criteria,10,cv.KMEANS_RANDOM_CENTERS)
-    # Now convert back into uint8, and make original image
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    res2 = res.reshape((img.shape))
-    '''
-    # 밑은 디버깅 상황 아니면 주석처리 바람
-    '''
-    cv.imshow('res2',img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    '''
     #여기까지
     avg = 0
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = gray.ravel() # 회색 이미지로 변경
     summation = 0 # 변수 선언
     #430, 582
-    unique, counts = np.unique(np.array([gray[each*lp+960] for each in range(1080)]), return_counts=True)
+    unique, counts = np.unique(np.array([gray[each*lp+960] for each in range(lp)]), return_counts=True)
     indi1 = dict(zip(unique, counts))# 특정 값의 픽셀의 갯수를 나타내는 딕셔너리
-    unique, counts = np.unique(np.array([gray[each*lp+430] for each in range(1080)]), return_counts=True)
+    unique, counts = np.unique(np.array([gray[each*lp+430] for each in range(lp)]), return_counts=True)
     indi2 = dict(zip(unique, counts))
+    print(indi1)
     for key, value in indi1.items():
         avg += key*value
         summation += value
@@ -85,9 +67,9 @@ def PhotoAnalysis2(Dir):
     gray = gray.ravel() # 회색 이미지로 변경
     summation = 0 # 변수 선언
     #430, 582
-    unique, counts = np.unique(np.array([gray[each*lp+960] for each in range(1080)]), return_counts=True)
+    unique, counts = np.unique(np.array([gray[each*lp+960] for each in range(lp)]), return_counts=True)
     indi1 = dict(zip(unique, counts))# 특정 값의 픽셀의 갯수를 나타내는 딕셔너리
-    unique, counts = np.unique(np.array([gray[each*lp+430] for each in range(1080)]), return_counts=True)
+    unique, counts = np.unique(np.array([gray[each*lp+430] for each in range(lp)]), return_counts=True)
     indi2 = dict(zip(unique, counts))
     for key, value in indi1.items():
         avg += key*value
@@ -110,3 +92,21 @@ def PhotoAnalysis2(Dir):
         return 75
     else:
         return 100
+
+'''
+    Z = img.reshape((-1,3))
+    # convert to np.float32
+    Z = np.float32(Z)
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 10
+    ret,label,center=cv.kmeans(Z,K,None,criteria,10,cv.KMEANS_RANDOM_CENTERS)
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+    # 밑은 디버깅 상황 아니면 주석처리 바람
+    cv.imshow('res2',img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+'''
